1fd6d57ee6d59ebb6c9cf498c79464b2e97d2304
Yohai Wiener <yohaiwiener@gmail.com>
2025-01-04 18:54:09 +0200

    basic twitter functionality

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,113 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { Pinecone } from '@pinecone-database/pinecone'
+import { Scraper } from '@the-convocation/twitter-scraper'
+import { getFirestore } from 'firebase-admin/firestore'
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import { onDocumentCreated } from 'firebase-functions/firestore'
+import OpenAI from 'openai'
+import { defineSecret } from 'firebase-functions/params'
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const pineconeApiKey = defineSecret('PINECONE_API_KEY')
+const openaiApiKey = defineSecret('OPENAI_API_KEY')
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp()
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+type TweetData = {
+  tweetId: string
+  createdAt: string
+  text: string
+  authorUsername: string
+  authorId: string
+}
+
+export const createTwitterDocument = functions.https.onCall<{ url: string }>(async (data, context) => {
+  console.log('Creating Twitter document', data)
+  const tweetId = data.data.url.split('/').pop()
+  console.log('Tweet ID:', tweetId)
+  if (!tweetId) {
+    throw new functions.https.HttpsError('invalid-argument', 'No tweet ID found')
+  }
+  const scraper = new Scraper()
+  const tweetData = await scraper.getTweet(tweetId)
+  if (!tweetData) {
+    throw new functions.https.HttpsError('invalid-argument', 'No tweet data found')
+  }
+
+  const document = {
+    tweetId,
+    createdAt: tweetData.timestamp,
+    text: tweetData.text,
+    authorUsername: tweetData.username,
+    authorId: tweetData.userId,
+  }
+
+  const firestore = getFirestore()
+  await firestore.collection('tweets').doc(tweetId).set(document)
+  return { success: true }
+})
+
+export const onTweetCreated = onDocumentCreated('/tweets/{tweetId}', async event => {
+  try {
+    if (!event.data) {
+      console.log('No tweet data found')
+      return
+    }
+    const pinecone = new Pinecone({ apiKey: pineconeApiKey.value() })
+    const openai = new OpenAI({
+      apiKey: openaiApiKey.value(),
+    })
+
+    const tweetData = event.data.data() as TweetData
+
+    const emb = await openai.embeddings.create({
+      model: 'text-embedding-ada-002',
+      input: tweetData.text,
+    })
+
+    const index = pinecone.index('tweets-vector-index')
+    await index.upsert([
+      {
+        id: tweetData.tweetId,
+        values: emb.data[0].embedding,
+        metadata: {
+          text: tweetData.text,
+          authorUsername: tweetData.authorUsername,
+          authorId: tweetData.authorId,
+          createdAt: tweetData.createdAt,
+        },
+      },
+    ])
+
+    console.log('Tweet created and indexed')
+  } catch (error) {
+    console.error('Error creating tweet and indexing', error)
+    throw error
+  }
+})
+
+export const searchTweets = functions.https.onCall<{ prompt: string }>(async (data, context) => {
+  const prompt = data.data.prompt
+  if (!prompt) {
+    throw new functions.https.HttpsError('invalid-argument', 'No prompt found')
+  }
+  const openai = new OpenAI({
+    apiKey: openaiApiKey.value(),
+    dangerouslyAllowBrowser: true,
+  })
+  const pinecone = new Pinecone({ apiKey: pineconeApiKey.value() })
+  const index = pinecone.index('tweets-vector-index')
+
+  const embededPrompt = await openai.embeddings.create({
+    input: prompt,
+    model: 'text-embedding-ada-002',
+  })
+
+  const queryVector = embededPrompt.data[0].embedding
+  const queryResponse = await index.query({
+    topK: 10,
+    includeMetadata: true,
+    vector: queryVector,
+  })
+
+  return queryResponse
+})
