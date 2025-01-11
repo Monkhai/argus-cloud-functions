31c0be73d77123c4c517e6ddd4ba4f4bb3e0768c
Yohai Wiener <yohaiwiener@gmail.com>
2025-01-11 19:52:56 +0200

    update to include index

---
+++ functions/src/index.ts
@@ -0,0 +1,28 @@
+import * as admin from 'firebase-admin'
+import { deleteResourceFn } from './resources/deleteResource'
+import { getRecentResourcesFn } from './resources/getRecentResources'
+import { onResourceCreatedFn } from './resources/onResouceCreated'
+import { searchResourcesFn } from './resources/searchResources'
+import { updateResourceFn } from './resources/updateResrouce'
+import { onUserCreatedFn } from './users/onUserCreated'
+import { onUserDeletedFn } from './users/onUserDeleted'
+import { createResourceDocumentFn } from './resources/createResourceDocument'
+
+admin.initializeApp()
+
+// Triggers
+export const onUserCreated = onUserCreatedFn
+export const onUserDeleted = onUserDeletedFn
+export const onResourceCreated = onResourceCreatedFn
+
+// CALLABLE FUNCTIONS
+//// resources
+export const createResourceDocument = createResourceDocumentFn
+export const updateResource = updateResourceFn
+export const deleteResource = deleteResourceFn
+
+//// Pinecone
+export const searchResources = searchResourcesFn
+
+//// Firestore
+export const getRecentResources = getRecentResourcesFn
